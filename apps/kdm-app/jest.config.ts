25441a3762126dfc9d6dbcbef2f409142c5827ae
Su Anli <su.anli@anlitz.com>
2024-06-29 01:23:01 +0800

    chore(nx): update ci script with format:write

--- apps/kdm-app/jest.config.ts
+++ apps/kdm-app/jest.config.ts
@@ -3,16 +3,21 @@ module.exports = {
   displayName: 'kdm',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
   moduleNameMapper: {
-    '\\.svg$': '@nx/react-native/plugins/jest/svg-mock'
+    '\\.svg$': '@nx/react-native/plugins/jest/svg-mock',
   },
   passWithNoTests: true,
   preset: 'react-native',
   resolver: '@nx/jest/plugins/resolver',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
-    '^.+.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve('react-native/jest/assetFileTransformer.js'),
-    '^.+.(js|ts|tsx)$': ['babel-jest', {
-      configFile: __dirname + '/.babelrc.js'
-    }]
-  }
+    '^.+.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
+      'react-native/jest/assetFileTransformer.js'
+    ),
+    '^.+.(js|ts|tsx)$': [
+      'babel-jest',
+      {
+        configFile: __dirname + '/.babelrc.js',
+      },
+    ],
+  },
 };
--- apps/kdm-app/src/app/app.tsx
+++ apps/kdm-app/src/app/app.tsx
@@ -1,13 +1,15 @@
-import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper"
-import { OnboardingPage } from "../pages"
-import { useColorScheme } from "react-native";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
+import { OnboardingPage } from '../pages';
+import { useColorScheme } from 'react-native';
 
 export const App = () => {
-  const theme = useTheme()
-  return <PaperProvider theme={theme}>
-    <OnboardingPage />
-  </PaperProvider>
-}
+  const theme = useTheme();
+  return (
+    <PaperProvider theme={theme}>
+      <OnboardingPage />
+    </PaperProvider>
+  );
+};
 
 const useTheme = () => {
   const colorScheme = useColorScheme();
